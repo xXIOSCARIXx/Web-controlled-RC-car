5c7327eb865d3342b4532480559a77a5b3720015
xXIOSCARIXx <31468960+xXIOSCARIXx@users.noreply.github.com>
2026-07-30 02:33:34 +0200

    Steering smoothing and speed limit

--- firmware.ino
+++ firmware.ino
@@ -9,6 +9,11 @@ const int ESC_NEUTRAL_US = 1500;
 const unsigned long COMMAND_TIMEOUT = 500;
 const unsigned long ARM_HOLD_MS = 2000;
 
+const float STEERING_EXPO = 0.80f;      // 0.80f = 80% smoothing
+const float THROTTLE_EXPO = 0.0f;       // 0.0f  = 0%  smoothing
+const float ESC_FORWARD_LIMIT = 0.25f;  // 0.25f = 25% of max speed
+const float ESC_BRAKE_LIMIT = 0.55f;    // 0.55f = 55% of max braking
+
 Servo steerServo;
 Servo escServo;
 
@@ -20,6 +25,30 @@ bool escArmed = false;
 
 unsigned long armHoldStart = 0;
 unsigned long lastCommandTime = 0;
+
+float applyExpo(float x, float expo) {
+  return (1.0f - expo) * x + expo * x * x * x;
+}
+
+int applyExpoToPWM(byte input, bool reverse, float expo, float forwardLimit, float brakeLimit) {
+  float x = ((float)input - 127.5f) / 127.5f;
+
+  x = applyExpo(x, expo);
+
+  if (reverse) {
+    x = -x;
+  }
+
+  if (x > 0) {
+    x *= forwardLimit;
+  } else {
+    x *= brakeLimit;
+  }
+
+  int pwm = (int)(1500.0f + x * 500.0f);
+
+  return constrain(pwm, 1000, 2000);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -39,7 +68,8 @@ void setup() {
 void loop() {
   readCommands();
 
-  linkAlive = lastCommandTime != 0 && millis() - lastCommandTime < COMMAND_TIMEOUT;
+  linkAlive = lastCommandTime != 0 &&
+              millis() - lastCommandTime < COMMAND_TIMEOUT;
 
   if (!linkAlive) {
     steerTargetUs = STEER_CENTER_US;
@@ -74,6 +104,7 @@ void readCommands() {
       if (Serial.available() < 4) break;
 
       Serial.read();
+
       byte steerByte = Serial.read();
       byte throttleByte = Serial.read();
       byte checksum = Serial.read();
@@ -84,10 +115,22 @@ void readCommands() {
 
       lastCommandTime = millis();
 
-      steerTargetUs = map(steerByte, 0, 255, 2000, 1000);
+      steerTargetUs = applyExpoToPWM(
+        steerByte,
+        true,
+        STEERING_EXPO,
+        1.0f,
+        1.0f
+      );
 
       if (escArmed) {
-        escTargetUs = map(throttleByte, 0, 255, 1000, 2000);
+        escTargetUs = applyExpoToPWM(
+          throttleByte,
+          false,
+          THROTTLE_EXPO,
+          ESC_FORWARD_LIMIT,
+          ESC_BRAKE_LIMIT
+        );
       } else {
         escTargetUs = ESC_NEUTRAL_US;
       }
